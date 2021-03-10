5c6bb6c1c32c51723b2c1fce534721fd94574c65
Yumi Shikata <yumishika17@gmail.com>
2021-03-10 18:24:01 +0100

    Push

---
+++ main.js
@@ -0,0 +1,55 @@
+//CREARE UN OGGETTO 
+
+var studenteSingolo = {
+    "nome": 'Yumi',
+    "cognome": 'Shikata',
+    "età": 25
+}
+
+for(var key in studenteSingolo){
+    console.log(studenteSingolo[key]);
+}
+
+var studenti = 
+[
+    {
+        "nome": 'Nami',
+        "cognome": 'Shikata',
+        "età": '4'
+
+    },
+    {
+        "nome": 'Sissi',
+        "cognome": 'Shikata',
+        "età": '14'
+
+    },
+    {
+        "nome": 'Kenta',
+        "cognome": 'Shikata',
+        "età": '20'
+
+    },
+    {
+        "nome": 'Norio',
+        "cognome": 'Shikata',
+        "età": '59'
+
+    }
+]
+
+for(var key in studenti){
+    console.log(studenti[key]);
+}
+
+var nuovoStudente = {
+    "nome": prompt('Inserisci il tuo nome: '),
+    "cognome": prompt('Inserisci il tuo cognome: '),
+    "età": prompt('Inserisci la tua età: ')
+}
+
+studenti.push(nuovoStudente);
+
+for (var key in studenti){
+    console.log(studenti[key]);
+}
